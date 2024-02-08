1ce88471f7e1fcbbdda18f22f41706f34ab11eeb
Peter M <psm@384.co>
2024-02-08 11:38:19 -0800

    documenting and debugging 'getting started' process

--- dist/snackabra.js
+++ dist/snackabra.js
@@ -271,7 +271,7 @@ export class SBError extends Error {
     constructor(message) {
         super(message);
         this.name = this.constructor.name;
-        if (Error.captureStackTrace)
+        if (typeof Error.captureStackTrace === 'function')
             Error.captureStackTrace(this, this.constructor);
         else
             this.stack = (new Error(message)).stack;
--- src/snackabra.ts
+++ src/snackabra.ts
@@ -639,8 +639,8 @@ export class SBError extends Error {
   constructor(message: string) {
     super(message);
     this.name = this.constructor.name;
-    if (Error.captureStackTrace)
-      Error.captureStackTrace(this, this.constructor);
+    if (typeof (Error as any).captureStackTrace === 'function')
+      (Error as any).captureStackTrace(this, this.constructor);
     else
       this.stack = (new Error(message)).stack;
     if (DBG) {
